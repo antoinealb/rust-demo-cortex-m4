df8d0693976b349ceb10a4a4b455905b20753a94
Antoine Albertelli <antoinea101@gmail.com>
2014-04-19 18:28:08 +0200

    Added an example about systick.

--- main.c
+++ main.c
@@ -8,12 +8,27 @@
 #include "inc/hw_types.h"
 #include "driverlib/gpio.h"
 #include "driverlib/sysctl.h"
+#include "driverlib/systick.h"
 #include "driverlib/interrupt.h"
 #include "driverlib/timer.h"
 
 #define LED_RED GPIO_PIN_1
 #define LED_BLUE GPIO_PIN_2
 #define LED_GREEN GPIO_PIN_3
+
+int i;
+
+void systick_handler(void)
+{
+    i++;
+    if (i==1000)
+        GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_BLUE|LED_GREEN, 0);
+
+    if (i==2000) {
+        GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_BLUE|LED_GREEN, LED_GREEN);
+        i = 0;
+    }
+}
 
 
 
@@ -22,15 +37,14 @@ int main(void)
 {
     SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+
     GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, LED_RED|LED_BLUE|LED_GREEN);
 
+    SysTickEnable();
+    SysTickPeriodSet(80 * 1000); /* every ms */
+    SysTickIntEnable();
+    SysTickIntRegister(systick_handler);
+
     for (;;) {
-        // set the red LED pin high, others low
-        GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, LED_RED);
-        SysCtlDelay(5000000);
-        GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, LED_BLUE);
-        SysCtlDelay(5000000);
-        GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, 0);
-        SysCtlDelay(5000000);
     }
 }
