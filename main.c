32d4b267a78bc182d37501f31efb2c5bba7e287b
Antoine Albertelli <antoinea101@gmail.com>
2014-04-19 13:36:53 +0200

    Cleanup main.c

--- main.c
+++ main.c
@@ -43,15 +43,12 @@
 #include "driverlib/interrupt.h"
 #include "driverlib/timer.h"
 
-// Basically here I'm checking that everything works fine.
-volatile unsigned long count;
-long var_init = 2;
-
 // An interrupt function.
 void Timer1A_ISR(void);
 
 // main function.
-int main(void) {
+int main(void)
+{
 	SysCtlClockSet(SYSCTL_SYSDIV_2_5|SYSCTL_USE_PLL|SYSCTL_OSC_MAIN|SYSCTL_XTAL_16MHZ);
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER1);
 	TimerConfigure(TIMER1_BASE, TIMER_CFG_SPLIT_PAIR | TIMER_CFG_A_PERIODIC);
@@ -61,9 +58,7 @@ int main(void) {
 	TimerIntEnable(TIMER1_BASE, TIMER_TIMA_TIMEOUT);
 	count=3;
 	TimerEnable(TIMER1_BASE, TIMER_A);
-    while(1)
-    {	
-		var_init++;
+    while (1) {
     }
 }
 
