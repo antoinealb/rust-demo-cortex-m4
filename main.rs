92093c3cec7f6a0cf272fec20273e23c9eadff21
Antoine Albertelli <antoinea101@gmail.com>
2015-04-30 23:20:13 +0200

    Implement blinking LED demo

--- main.rs
+++ main.rs
@@ -6,15 +6,37 @@
 #![crate_type="staticlib"]
 
 extern crate core;
-mod sysctl;
-mod runtime;
 
+mod runtime;
+mod sysctl;
+mod led_driver;
+mod gpio;
+
+fn clock_init() -> () {
+    let clock_config = sysctl::SYSCTL_SYSDIV_2_5 + sysctl::SYSCTL_USE_PLL +
+                       sysctl::SYSCTL_XTAL_16MHZ + sysctl::SYSCTL_OSC_MAIN;
+    sysctl::clock_set(clock_config);
+}
 
 
 #[no_mangle] pub fn main()
 {
-    let clock_config = sysctl::SYSCTL_SYSDIV_2_5 + sysctl::SYSCTL_USE_PLL +
-                       sysctl::SYSCTL_XTAL_16MHZ + sysctl::SYSCTL_OSC_MAIN;
-    sysctl::clock_set(clock_config);
+    clock_init();
+    led_driver::led_init();
 
+    loop {
+        let mut i = 0;
+        while i < 1000000 {
+            i += 1;
+            led_driver::set_red(false);
+        }
+
+        i = 0;
+
+        while i < 1000000 {
+            i += 1;
+            led_driver::set_red(true);
+        }
+    }
 }
+
