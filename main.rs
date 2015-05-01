2c274f8c2df08f9db8d778dcc3983e0260ee9153
Antoine Albertelli <antoinea101@gmail.com>
2015-05-01 11:14:32 +0200

    Cleanup main

--- main.rs
+++ main.rs
@@ -13,15 +13,13 @@ mod led_driver;
 mod gpio;
 mod systick;
 
-fn clock_init() -> () {
+fn clock_init() {
     let clock_config = sysctl::SYSCTL_SYSDIV_2_5 + sysctl::SYSCTL_USE_PLL +
                        sysctl::SYSCTL_XTAL_16MHZ + sysctl::SYSCTL_OSC_MAIN;
     sysctl::clock_set(clock_config);
 }
 
-static mut tick_counter: u32 = 0;
-
-fn handler() -> () {
+fn handler() {
 }
 
 
